e99c58ed6734156f20fb9c47cef44449536c7784
King Elisha <ek.chibueze@gmail.com>
2020-08-19 16:58:15 +0100

    feat: implemented facebook strategy

---
+++ src/facebook.strategy.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy } from 'passport-facebook';
+
+@Injectable()
+export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
+  constructor() {
+    super({
+      clientID: process.env.APP_ID,
+      clientSecret: process.env.APP_SECRET,
+      callbackURL: 'http://localhost:3000/facebook/redirect',
+      profileFields: ['email', 'name'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: (err: any, user: any, info?: any) => void,
+  ): Promise<any> {
+    const { name, emails } = profile;
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+    };
+    const payload = {
+      user,
+      accessToken,
+    };
+
+    done(null, payload);
+  }
+}
