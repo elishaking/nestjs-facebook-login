20d222055b2315206ec8c7455557df0290644fa1
King Elisha <ek.chibueze@gmail.com>
2020-08-19 13:09:30 +0100

    chore: setup env variables

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
